f70afe43c2a01d0411d50ee61e4bb61a43b106c4
sky9154 <sky0932039612@gmail.com>
2024-05-01 05:23:31 +0800

    打包專案

--- API/main.py
+++ API/main.py
@@ -1,7 +1,9 @@
 import os
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from dotenv import load_dotenv
 from api.user import router as user_router
 from api.event import router as event_router
@@ -24,9 +26,25 @@ def create_app():
   return app
 
 app = create_app()
+
 app.include_router(user_router, prefix='/api/user')
 app.include_router(event_router, prefix='/api/event')
 app.include_router(role_router, prefix='/api/role')
+
+
+@app.get('/static/{full_path:path}')
+async def static(request: Request, full_path):
+  return FileResponse(f'dist/static/{full_path}')
+
+@app.get('/images/{full_path:path}')
+async def images(request: Request, full_path):
+  return FileResponse(f'dist/images/{full_path}')
+
+@app.get('/{full_path:path}')
+async def pages():
+  return FileResponse('dist/index.html')
+
+app.mount('/', StaticFiles(directory='dist'))
 
 if __name__ == '__main__':
   API_HOST = os.getenv('API_HOST')
